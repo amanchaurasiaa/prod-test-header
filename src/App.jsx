5fa039f56f19b12af8d80ebec17a83987b09bb8f
aman chaurasia <aman.ch234@gmail.com>
2021-12-07 14:27:46 +0530

    header

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
+import Header from "./Header";
 import "./index.css";
 
-const App = () => <div>Hi there, I'm React from Webpack 5.</div>;
+const App = () => (
+    <div>
+        <Header />
+        <div>Hi there, I'm React from Webpack 5.</div>
+    </div>
+)
 
 ReactDOM.render(<App />, document.getElementById("app"));
--- webpack.config.js
+++ webpack.config.js
@@ -42,10 +42,12 @@ module.exports =(_, argv) =>  ({
 
   plugins: [
     new ModuleFederationPlugin({
-      name: "starter",
+      name: "header",
       filename: "remoteEntry.js",
       remotes: {},
-      exposes: {},
+      exposes: {
+        "./Header": "./src/Header"
+      },
       shared: {
         ...deps,
         react: {
